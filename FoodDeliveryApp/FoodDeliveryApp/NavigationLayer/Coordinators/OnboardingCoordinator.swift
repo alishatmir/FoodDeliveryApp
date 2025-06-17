a3bce42b131d69cd06eb995c8118bc6c4f0d8194
Alina Tikhomirova <alinatmir@gmail.com>
2025-06-17 18:16:54 +0700

    Add onboarding screen

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -10,12 +10,32 @@ import UIKit
 class OnboardingCoordinator: Coordinator {
     
     override func start() {
-        let vc = ViewController()
-        navigationController?.pushViewController(vc, animated: true)
-        
+        showOnboarding()
     }
     
     override func finish() {
         print("AppCoordinator finish")
+    }
+}
+
+private extension OnboardingCoordinator {
+    func showOnboarding() {
+        var pages = [UIViewController]()
+        
+        let firstVC = UIViewController()
+        firstVC.view.backgroundColor = .purple
+        
+        let secondVC = UIViewController()
+        secondVC.view.backgroundColor = .gray
+        
+        let thirdVC = UIViewController()
+        thirdVC.view.backgroundColor = .green
+        pages.append(firstVC)
+        pages.append(secondVC)
+        pages.append(thirdVC)
+        
+        let presenter = OnboardingViewPresenter(coordinator: self)
+        let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
+        navigationController?.pushViewController(viewController, animated: true)
     }
 }
---
+++ FoodDeliveryApp/FoodDeliveryApp/UserStories/Onboarding/OnboardingViewPresenter.swift
@@ -0,0 +1,26 @@
+//
+//  OnboardingViewPresenter.swift
+//  FoodDeliveryApp
+//
+//  Created by Алина Тихомирова on 17.06.2025.
+//
+
+import Foundation
+
+protocol OnboardingViewOutput: AnyObject {
+    func onboardingFinish()
+}
+
+class OnboardingViewPresenter: OnboardingViewOutput {
+    weak var coordinator: OnboardingCoordinator!
+    
+    init(coordinator: OnboardingCoordinator!) {
+        self.coordinator = coordinator
+    }
+    
+    func onboardingFinish() {
+        coordinator.finish()
+    }
+    
+    
+}
